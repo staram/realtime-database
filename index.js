9971bb1b3bff5f4f9b266a76fcabc7ba31a9865f
staram <10952725+staram@users.noreply.github.com>
2024-01-22 12:17:45 +0000

    Add justified flexbox for items
    Apply flexbox and flex-grow to shopping cart items

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-30624-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -15,10 +15,28 @@ const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-
+    
     push(shoppingListInDB, inputValue)
+    
+    clearInputFieldEl()
 
-    inputFieldEl.value = ""
-
-    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
+    appendItemToShoppingListEl(inputValue)
 })
+
+
+onValue(shoppingListInDB, function(snapshot) {
+    let itemsArray = Object.values(snapshot.val())
+    console.log(itemsArray)
+
+    for (let i = 0; i<itemsArray.length; i++) {
+        appendItemToShoppingListEl(itemsArray[i])
+    }
+})
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
